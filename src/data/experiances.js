8b00a72fe339d6bdaa49fb1fc799278598854e5f
antho <anthonynuge0509@gmail.com>
2025-01-25 16:38:39 -0600

    Add experiances section.

---
+++ src/data/experiances.js
@@ -0,0 +1,20 @@
+export const experiances = [
+  {
+    company: "Aesyn",
+    role: "Software Developer Intern",
+    description:
+      "Developed and designed user analytics page allowing bussiness to view influencer metrics for marketing campaigns. Connect social media APIs to pull user data and aggregate into meaningful insights.",
+    image: "",
+    link: "/",
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "React",
+      "JavaScript",
+      "MongoDB",
+      "TailWindCSS",
+    ],
+    start: "July 2024",
+    end: "Present",
+  },
+];
