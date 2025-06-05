74299828b7aa8911f83b68d33b58138caa372e15
antho <anthonynuge0509@gmail.com>
2025-06-04 19:06:27 -0500

    Update experience to to render data dynamically. Fix minor spacing for mobile.

--- src/pages/Experiance.jsx
+++ src/pages/Experiance.jsx
@@ -15,24 +15,47 @@ const Experiance = () => {
         </div>
       </div>
       {/* Main Content */}
-      <div className="grow mt-4">
-        <div className="grid relative">
-          <img src="/images/AesynPage.png" alt="Work Home Page" />
-        </div>
-        {/* content */}
-        <div className="mt-4">
-          <div className="flex flex-col sm:flex-row justify-between gap-0 sm:gap-1 sm:items-center">
-            <h3 className="text-lg item">{experiances[0].company} <span className="text-xs">- {experiances[0].role}</span></h3>
-            <p className="text-xs dark:text-white">{experiances[0].start} - {experiances[0].end}</p>
+      <div className="grow mt-4 space-y-16">
+        {experiances.map((experience, index) => (
+          <div key={index}>
+            <div className="grid relative">
+              <a
+                href={experience.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={experience.image} alt="Company Cover Image" />
+              </a>
+            </div>
+            {/* content */}
+            <div className="mt-4">
+              <div className="flex flex-col sm:flex-row justify-between gap-0 sm:gap-1 sm:items-center">
+                <a
+                  href={experience.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <h3 className="text-lg item">
+                    {experience.company}{" "}
+                    <span className="text-xs">- {experience.role}</span>
+                  </h3>
+                </a>
+                <p className="text-xs dark:text-white">
+                  {experience.start} - {experience.end}
+                </p>
+              </div>
+              <p className="text-sm mb-4 mt-1">{experience.summary}</p>
+              <ul className="text-sm list-disc pl-5 mt-1">
+                {experiances[0].points.map((point, index) => (
+                  <li key={index} className="mb-1">
+                    {point}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <p className="text-sm mb-4">{experiances[0].summary}</p>
-          <ul className="text-sm list-disc pl-5 mt-1">
-            {experiances[0].points.map((point, index) => (
-              <li key={index} className="mb-1">{point}</li>
-            ))}
-          </ul>
-        </div>
-      </div >
+        ))}
+      </div>
     </>
   );
 };
